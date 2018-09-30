0802327dc5b44643844733e65c6e5e853465891a
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-09-30 13:57:14 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,13 @@
+var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+}
+
 function doWhileLoop(array){
+  do{
+    array.pop();
+  }while(array.length > 0 && incrementVariable());
   
+  return array;
 }
